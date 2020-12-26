92d81741346f838ee3a3c259e894a308846537b7
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-12-26 22:25:18 +0200

    Dz

--- src/js/countries.js
+++ src/js/countries.js
@@ -1,7 +1,82 @@
-import fetchCountries from './fetchCountries'
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries.js';
+import listOfContriesTpl from '../templates/list-of-countries.hbs';
+import countryCardTpl from '../templates/country-markup.hbs';
 
-fetchCountries()
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
-const coutr = 'fre' 
+const inputEl = document.querySelector('.input-search');
+const cardContainer = document.querySelector('.js-card-container');
+let countryToSearch = '';
 
-fetchCountries(coutr)
+inputEl.addEventListener(
+  'input',
+  debounce(() => {
+    onSearch();
+  }, 500),
+);
+
+function onSearch() {
+  countryToSearch = inputEl.value;
+  console.log(countryToSearch);
+
+  if (!countryToSearch) {
+    clearMarkup();
+    return;
+  }
+
+  fetchCountries(countryToSearch)
+    .then(checkingNumberOfCountries)
+    .catch(onFetchError);
+}
+
+function checkingNumberOfCountries(countries) {
+  if (countries.length > 10) {
+    clearMarkup();
+    tooManyCountries();
+  } else if (countries.length <= 10 && countries.length > 1) {
+    clearMarkup();
+    renderMarkup(listOfContriesTpl, countries);
+  } else if (countries.length === 1) {
+    clearMarkup();
+    renderMarkup(countryCardTpl, countries[0]);
+  } else {
+    clearMarkup();
+    noResult();
+  }
+}
+
+function renderMarkup(template, countries) {
+  const markup = template(countries);
+  cardContainer.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearMarkup() {
+  cardContainer.innerHTML = '';
+}
+
+function noResult() {
+  info({
+    title: 'Ой!',
+    text: 'Нет данных!',
+    delay: 1500,
+    closerHover: true,
+  });
+}
+
+function tooManyCountries() {
+  error({
+    title: 'Ошибка!',
+    text: 'введите запрос более конкретный!',
+    delay: 2500,
+    closerHover: true,
+  });
+}
+
+function onFetchError(error) {
+  clearMarkup();
+
+  console.log(error);
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,10 @@
 
 
 function fetchCountries(searchQuery) {
-    const url = 'https://restcountries.eu/rest/v2/name/searchQuery'
+    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
     
     return fetch(url)
-        .then(res => res.json())
-        .then(({ name }) => name);
+        .then(res => res.json());
 };
 
 export default  fetchCountries
